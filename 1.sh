4ac06d46928afe662ed4d6c19e6a32fe78117bc1
Ubuntu <ubuntu@ip-172-31-4-231.sa-east-1.compute.internal>
2022-11-22 18:51:04 +0000

    Costos de (1) y (4) en performance.txt

--- 1.sh
+++ 1.sh
@@ -3,7 +3,7 @@
 re_scale() {
     local INPUT_FILE=$1
     local SCALE=$2 # width:height format, example 800:600
-    ffmpeg -i "$INPUT_FILE" -vf scale="$SCALE" -c:a copy "$(tr : x <<<$SCALE)_$INPUT_FILE"
+    ffmpeg -i "$INPUT_FILE" -vf scale="$SCALE" -c:a copy "$(echo $SCALE | tr : x)_$INPUT_FILE"
 }
 
 re_scale $1 $2
